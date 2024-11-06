a2de3d7fd85685f768630c439bb82b21facd330e
shu qi <shu@fetch.ly>
2024-11-06 19:43:30 +0700

    when delete a user, delete the completed or processing rentals too

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :rentals
+    has_many :rentals, dependent: :destroy
     validates :name, presence: true
     validates :email, uniqueness: true, presence: true, format: { with: /\A([^\s]?[\w\-+.\/"\%\!]+)@([A-Za-z\d\-\[\]\.\:\(\)]+)\z/i }, on: :create
     before_destroy { errors.add(:rental, "#{name} has rentals") && throw(:abort) if rentals.exists?(status: :rent_out) }
