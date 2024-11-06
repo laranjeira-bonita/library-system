4be1614884cdfb92a943fe6f14e15b74a9d2d8d1
shu qi <shu@fetch.ly>
2024-11-06 19:41:43 +0700

    show not allow to delete user if the user currently has a book not returned

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :rentals
     validates :name, presence: true
     validates :email, uniqueness: true, presence: true, format: { with: /\A([^\s]?[\w\-+.\/"\%\!]+)@([A-Za-z\d\-\[\]\.\:\(\)]+)\z/i }, on: :create
+    before_destroy { errors.add(:rental, "#{name} has rentals") && throw(:abort) if rentals.exists?(status: :rent_out) }
 end
